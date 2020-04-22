d0f90dc849dbf4539ad53cf94a5b698b07a50a11
Antonio <csalazartenorio@gmail.com>
2020-04-21 20:30:48 -0500

    v1.0

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import sys
+import os
+
 from tkinter import *
 from time import *
+
+try:
+    from tkinter import ttk
+except:
+    import ttk
 
 from HillClimbing import *
 
@@ -54,11 +62,14 @@ class PageOne(Frame):
 
         labelError = Label(self, fg="red")
 
-        labelQuantity = Label(self, text="Cantidad: ")
-        labelQuantity.grid(row=0, column=0)
+        labelInstruction = Label(self, text="Ingrese la cantidad de Personas y su Velocidades")
+        labelInstruction.grid(row=0, column=0, columnspan=2)
+
+        labelQuantity = Label(self, text="Cantidad Personas: ")
+        labelQuantity.grid(row=1, column=0)
 
         entryQuantity = Entry(self)
-        entryQuantity.grid(row=0, column=1)
+        entryQuantity.grid(row=1, column=1)
 
         def getVelocity():
             persons.clear()
@@ -87,7 +98,7 @@ class PageOne(Frame):
                 controller.show_frame("PageTwo")
                 controller.create_persons()
 
-        buttonPrint = Button(self, text="Init", command=init)
+        buttonPrint = ttk.Button(self, text="Siguiente", command=init)
 
         def clearVelocity():
             for i in range(len(entriesVelocity)):
@@ -104,17 +115,17 @@ class PageOne(Frame):
             labels = [Label(self, text="Velocidad %s:" % (i + 1)) for i in range(quantity)]
 
             for i in range(quantity):
-                rowIndex = 3 + i
+                rowIndex = 4 + i
                 labels[i].grid(row=rowIndex, column=0)
                 entries[i].grid(row=rowIndex, column=1)
                 entriesVelocity.append(entries[i])
                 labelsVelocity.append(labels[i])
-            labelError.grid(row=(3 + quantity), column=0)
+            labelError.grid(row=(4 + quantity), column=0)
 
-            buttonPrint.grid(row=(4 + quantity), column=1)
+            buttonPrint.grid(row=(5 + quantity), column=1)
 
-        buttonCreate = Button(self, text="Create", command=createVelocity)
-        buttonCreate.grid(row=1, column=1)
+        buttonCreate = ttk.Button(self, text="Crear", command=createVelocity)
+        buttonCreate.grid(row=2, column=1)
 
 
 class PageTwo(Frame):
@@ -122,18 +133,34 @@ class PageTwo(Frame):
     personsText = []
     labels = {}
     canvas = None
+    buttonCreate = None
+
+    def restart(self):
+        python = sys.executable
+        os.execl(python, python, * sys.argv)
+
+    def end(self, total):
+        extra = "s"
+        if total <= 1:
+            extra = ""
+
+        labelEnd = Label(self, text="El menor tiempo para que todos cruzen el puente es de %s minuto%s" % (str(total), extra))
+        labelEnd.pack()
+
+        buttonRestart = ttk.Button(self, text="Reiniciar", command=self.restart)
+        buttonRestart.pack()
 
     def frameSleep(self, sec):
         Frame.update(self)
         sleep(sec)
 
     def createLabels(self):
-        labelValue = Label(root, text="Valor del Movimiento: 0")
-        labelCost = Label(root, text="Costo Actual: 0")
-        labelTotal = Label(root, text="Costo Total: 0")
-        labelValue.pack(fill=Y)
-        labelCost.pack(fill=Y)
-        labelTotal.pack(fill=Y)
+        labelValue = Label(self, text="Valor del Movimiento: 0")
+        labelCost = Label(self, text="Tiempo Actual: 0")
+        labelTotal = Label(self, text="Tiempo Total: 0")
+        labelValue.pack()
+        labelCost.pack()
+        labelTotal.pack()
 
         self.labels["value"] = labelValue
         self.labels["cost"] = labelCost
@@ -156,17 +183,21 @@ class PageTwo(Frame):
         extra = 0
         if quantity > 9:
             extra = (int((quantity - 9) / 3) * 30)
-        height = 100 + extra
+        height = 110 + extra
         print(height)
         _canvas = Canvas(self, width=350, height=height)
 
-        _canvas.create_line(100, 15, 220, 15)
-        _canvas.create_line(100, 75, 220, 75)
+        _canvas.create_text(50, 10, text="Lado Inseguro")
+        _canvas.create_text(300, 10, text="Lado Seguro")
+
+        bridge = _canvas.create_rectangle(100, 25, 230, 85)
+        _canvas.create_text(165, 95, text="Puente")
+        _canvas.itemconfig(bridge, fill="#3d1600")
 
         for i in range(quantity):
             person = str(persons[i])
             x = 10 + ((i % 3) * 30)
-            y = 10 + (int((i / 3)) * 30)
+            y = 20 + (int((i / 3)) * 30)
             box = _canvas.create_rectangle(x, y, x + 20, y + 20)
             text = _canvas.create_text(x + 10, y + 10, text=str(person))
             self.personsBox.append(box)
@@ -187,7 +218,7 @@ class PageTwo(Frame):
 
         for j in range(10):
             for i in range(len(toMove)):
-                y = 20 + (i * 30)
+                y = 30 + (i * 30)
                 x = pos + (add * j)
                 index = persons.index(toMove[i])
                 box = self.personsBox[index]
@@ -207,7 +238,7 @@ class PageTwo(Frame):
         for _ in toSet:
             index = persons.index(_)
             x = add + (10 + ((index % 3) * 30))
-            y = 10 + (int((index / 3)) * 30)
+            y = 20 + (int((index / 3)) * 30)
 
             box = self.personsBox[index]
             text = self.personsText[index]
@@ -227,132 +258,16 @@ class PageTwo(Frame):
         self.controller = controller
 
         def startAlgorithm():
-            hillClimbing(persons, self.movement, self.paint, self.changeText)
+            self.buttonCreate['state'] = 'disabled'
+            hillClimbing(persons, self.movement, self.paint, self.changeText, self.end)
 
-        buttonCreate = Button(self, text="Start", command=startAlgorithm)
-        buttonCreate.pack()
+        _buttonCreate = ttk.Button(self, text="Empezar", command=startAlgorithm)
+        _buttonCreate.pack()
+
+        self.buttonCreate = _buttonCreate
 
 
 if __name__ == "__main__":
     root = MainRoot()
+    root.title("The Microsoft Problem")
     root.mainloop()
-
-# topFrame = Frame(root)
-# topFrame.pack()
-# bottomFrame = Frame(root)
-# bottomFrame.pack(side="bottom")
-#
-# button1 = Button(topFrame, text="Button 1", fg="red")
-# button2 = Button(topFrame, text="Button 2", fg="green")
-# button3 = Button(bottomFrame, text="Button 3", fg="blue")
-#
-# button1.pack()
-# button2.pack()
-# button3.pack()
-
-
-# one = Label(root, text="One", bg="red", fg="white")
-# one.pack()
-#
-# two = Label(root, text="Two", bg="green", fg="blue")
-# two.pack(fill=X)
-#
-# three = Label(root, text="Three", bg="blue", fg="black")
-# three.pack(side=LEFT, fill=Y)
-
-#
-# label_1 = Label(root, text="Cantidad de Personas")
-# label_2 = Label(root, text="Velocidades")
-# entry_1 = Entry(root)
-# entry_2 = Entry(root)
-#
-# label_1.grid(row=0, column=0, sticky=E)
-# label_2.grid(row=2, column=0)
-#
-# entry_1.grid(row=0, column=1)
-# # entry_2.grid(row=1, column=1)
-#
-# # c = Checkbutton(root, text="Keep me logged in")
-# # c.grid(columnspan=2)
-#
-#
-# def printName():
-#     print("Personas: ", entry_1.get())
-#
-#
-# button_1 = Button(root, text="Print my name", command=printName)
-# # button_1 = Button(root, text="Print my name")
-# # button_1.bind("<Button-1>", printName)
-# button_1.grid(row=1, column=0)
-
-# def leftClick(event):
-#     print("Left")
-#
-#
-# def rightClick(event):
-#     print("Right")
-#
-#
-# frame = Frame(root, width=300, height=250)
-# frame.bind("<Button-1>", leftClick)
-# frame.bind("<Button-2>", rightClick)
-# frame.pack()
-
-
-# def doNothing():
-#     print("Nothing")
-#
-#
-# # Menu
-#
-# menu = Menu(root)
-# root.config(menu=menu)
-#
-# subMenu = Menu(menu)
-# menu.add_cascade(label="File",  menu=subMenu)
-# subMenu.add_command(label="New Project...", command=doNothing)
-# subMenu.add_command(label="Now...", command=doNothing)
-# subMenu.add_separator()
-# subMenu.add_command(label="Exit", command=doNothing)
-#
-# editMenu = Menu(menu)
-# menu.add_cascade(label="Edit",  menu=editMenu)
-# editMenu.add_command(label="Redo", command=doNothing)
-#
-# # Tool Bar
-#
-# toolBar = Frame(root, bg="Blue")
-#
-# insertButton = Button(toolBar, text="Insert Image", command=doNothing)
-# insertButton.pack(side=LEFT, padx=2, pady=2)
-#
-# printButton = Button(toolBar, text="Print", command=doNothing)
-# printButton.pack(side=LEFT, padx=2, pady=2)
-#
-# toolBar.pack(side=TOP, fill=X)
-#
-# # Status Bar
-#
-# status = Label(root, text="Preparing to do nothing...", bd=1, relief=SUNKEN, anchor=W)
-# status.pack(side=BOTTOM, fill=X)
-
-
-# import tkinter.messagebox
-#
-# tkinter.messagebox.showinfo("Window Title", "Monkeys can live up to 300 years.")
-#
-# answer = tkinter.messagebox.askquestion('Question 1', "Do you like?")
-#
-# if answer == "yes":
-#     print(':D')
-
-
-# canvas = Canvas(root, width=200, height=100)
-# canvas.pack()
-#
-# blackLine = canvas.create_line(0, 0, 200, 50)
-# redLine = canvas.create_line(0, 100, 200, 50, fill="red")
-# greenBox = canvas.create_rectangle(25, 25, 130, 60, fill="green")
-#
-# canvas.delete(redLine)
-# canvas.delete(ALL)
